fc8dc77aafa7142d538b5ee043c9b4f2cc991983
Matt Steele <mattsteele000@gmail.com>
2025-02-01 15:37:54 -0700

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,32 +4,41 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <h1>Hello World</h1>
+       <h1 className="text-8xl uppercase">Matt Steele</h1>
+       <h2 className="text-4xl">Software Engineer</h2>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="about"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Item One
+          About
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="/resume"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Item Two
+          Resume
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/mattwsteele"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Item 3
+          LinkedIn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://westerncode.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Western Code
         </a>
       </footer>
     </div>
