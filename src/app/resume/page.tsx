e37ecfa54c287bb0dbb22d075ab0b87163054d01
Matt Steele <mattsteele000@gmail.com>
2025-02-02 11:14:27 -0700

    Update footer links

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -6,13 +6,16 @@ export default function Resume() {
        <h1 className="text-8xl uppercase">Resume</h1>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+
+        <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/" rel="noopener noreferrer">Home</Link>
+
         <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/about" rel="noopener noreferrer">About</Link>
 
         <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/resume" rel="noopener noreferrer">Resume</Link>
 
         <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn</Link>
 
-        <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/westerncode" rel="noopener noreferrer">Western Code</Link>
+        <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="https://westerncode.com" rel="noopener noreferrer">Western Code</Link>
       </footer>
     </div>
   );
