b72e22bd532ad37967d193cb2c572bf7bde2e775
Matt Steele <mattsteele000@gmail.com>
2025-02-02 17:59:12 -0700

    Update footer styles

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,12 +2,12 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-      <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/" rel="noopener noreferrer">Home</Link>
-      <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/about" rel="noopener noreferrer">About</Link>
-      <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="/resume" rel="noopener noreferrer">Resume</Link>
-      <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn</Link>
-      <Link className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="https://westerncode.com" rel="noopener noreferrer">Western Code</Link>
+    <footer className="flex flex-col w-full">
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="/" rel="noopener noreferrer">Home</Link>
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="/about" rel="noopener noreferrer">About</Link>
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="/resume" rel="noopener noreferrer">Resume</Link>
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn</Link>
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="https://westerncode.com" rel="noopener noreferrer">Western Code</Link>
     </footer>
   );
 }
