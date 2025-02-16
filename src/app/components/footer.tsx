2530f4f0a3ab33878e83b05671c0c2d20fc71f23
Matt Steele <mattsteele000@gmail.com>
2025-02-16 12:08:24 -0700

    Update default colors

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,11 +3,15 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="flex flex-col w-full">
-      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="/" rel="noopener noreferrer">Home</Link>
-      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="/about" rel="noopener noreferrer">About</Link>
-      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="/resume" rel="noopener noreferrer">Resume</Link>
-      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn</Link>
-      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-2 border-indigo-600 border-solid hover:underline hover:underline-offset-4" href="https://westerncode.com" rel="noopener noreferrer">Western Code</Link>
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-b-2 border-solid hover:underline hover:underline-offset-4 bg-red-two" href="/" rel="noopener noreferrer">Home</Link>
+
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-b-2 border-solid hover:underline hover:underline-offset-4 bg-red-one" href="/about" rel="noopener noreferrer">About</Link>
+
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-b-2 border-solid hover:underline hover:underline-offset-4 bg-blue-two" href="/resume" rel="noopener noreferrer">Resume</Link>
+
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-b-2 border-solid hover:underline hover:underline-offset-4 bg-blue-one" href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn</Link>
+
+      <Link className="flex w-full justify-center py-2.5 uppercase items-center border-b-2 border-solid hover:underline hover:underline-offset-4 bg-gray-two" href="https://westerncode.com" rel="noopener noreferrer">Western Code</Link>
     </footer>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
     <>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-8xl uppercase">Matt Steele</h1>
-        <h2 className="text-4xl">Software Engineer</h2>
+        <h1 className="text-8xl uppercase text-blue-one">Matt Steele</h1>
+        <h2 className="text-4xl text-blue-one">Software Engineer</h2>
         </main>
       </div>
       <Footer />
