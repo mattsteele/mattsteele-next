156f277658a020b0e378572fd6afb243ed3da7f1
Matt Steele <mattsteele000@gmail.com>
2025-03-15 15:59:59 -0600

    Update resume page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,13 @@
 import Footer from '../components/footer';
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function About() {
   return (
     <>
       <div className="flex flex-col justify-center h-screen text-center">
 
-        <div className="relative m-auto size-120 bg-blue-one pt-2 pl-2">
+        <div className="relative size-120 bg-blue-one mx-auto">
           <div className="absolute top-10 left-10 text-left text-2xl">
             <h2>Matt Steele</h2>
             <h3>Software Engineer</h3>
@@ -20,14 +21,14 @@ export default function About() {
           />
         </div>
 
-        <div className="text-blue-one">
-          <p>I build &amp; maintain systems for designers &amp; developers. Much of my work is in component libraries, tokens, application prototyping, &amp; proof-of-concept work.</p>
+        <div className="max-w-4xl text-blue-one text-xl text-left mx-auto p-4">
+          <p className="py-4 leading-7">I build &amp; maintain systems for designers &amp; developers. Much of my work is in component libraries, tokens, application prototyping, &amp; proof-of-concept work.</p>
 
-          <p>My work is driven by <a href="https://www.interaction-design.org/literature/topics/ux-design">User Experience (UX)</a>,  <a href="https://www.invisionapp.com/inside-design/guide-to-design-systems/">Design Systems</a> &amp; <a href="https://www.ideou.com/pages/design-thinking">Design Thinking</a>.</p>
+          <p className="py-4 leading-7">My work is driven by <Link href="https://www.interaction-design.org/literature/topics/ux-design">User Experience (UX)</Link>,  <Link href="https://www.invisionapp.com/inside-design/guide-to-design-systems/">Design Systems</Link> &amp; <Link href="https://www.ideou.com/pages/design-thinking">Design Thinking</Link>.</p>
 
-          <p>I currently work as a Senior Software Engineer at Charter Communications in Denver, Colorado.</p>
+          <p className="py-4 leading-7">I currently work as a Senior Software Engineer at Charter Communications in Denver, Colorado.</p>
 
-          <p>You can reach me <a href="mailto:mattsteele@outlook.com">here</a>.</p>
+          <p className="py-4 leading-7">You can reach me <Link href="mailto:mattsteele000@gmail.com">here</Link>.</p>
 
           <div>
             <Image
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -3,10 +3,10 @@ import Footer from '../components/footer';
 export default function Resume() {
   return (
     <>
-      <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h1 className="text-8xl uppercase">Resume</h1>
-        </main>
+      <div className="flex flex-col justify-center h-screen text-center">
+        <div className="text-blue-one text-2xl">
+          <h1>RESUME</h1>
+        </div>
       </div>
       <Footer />
     </>
