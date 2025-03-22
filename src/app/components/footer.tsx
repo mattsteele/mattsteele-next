f8486d18a0fd8d0861a29299d5a0ceced90157d8
Matt Steele <mattsteele000@gmail.com>
2025-03-22 14:23:21 -0600

    Animate links

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -4,26 +4,35 @@ export default function Footer() {
   return (
     <footer className="flex flex-col w-full">
 
-      <div className="bg-red-two border-b-4 border-solid border-transparent hover:border-b-gray-300 transition-colors duration-500">
-        <Link className="flex w-full justify-center pt-2 uppercase items-center no-underline" href="/" rel="noopener noreferrer">Home</Link>
+      <div className="bg-red-two relative">
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/" rel="noopener noreferrer">Home
+          <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
+        </Link>
       </div>
 
-      <div className="bg-red-one border-b-4 border-solid border-transparent hover:border-b-gray-300 transition-colors duration-500">
-        <Link className="flex w-full justify-center pt-2 uppercase items-center " href="/about" rel="noopener noreferrer">About</Link>
+      <div className="bg-red-one relative">
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/" rel="noopener noreferrer">About
+          <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
+        </Link>
       </div>
 
-      <div className="bg-blue-two border-b-4 border-solid border-transparent hover:border-b-gray-300 transition-colors duration-500">
-        <Link className="flex w-full justify-center pt-2 uppercase items-center" href="/resume" rel="noopener noreferrer">Resume</Link>
+      <div className="bg-blue-two relative">
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/" rel="noopener noreferrer">Resume
+          <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
+        </Link>
       </div>
 
-      <div className="bg-blue-one border-b-4 border-solid border-transparent hover:border-b-gray-300 transition-colors duration-500">
-        <Link className="flex w-full justify-center pt-2 uppercase items-center " href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn</Link>
+      <div className="bg-blue-one relative">
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="https://www.linkedin.com/in/mattwsteele" rel="noopener noreferrer">LinkedIn
+          <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
+        </Link>
       </div>
 
-      <div className="bg-gray-two border-b-4 border-solid border-transparent hover:border-b-gray-300 transition-colors duration-500">
-        <Link className="flex w-full justify-center pt-2 uppercase items-center" href="https://westerncode.com" rel="noopener noreferrer">Western Code</Link>
+      <div className="bg-gray-two relative">
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="https://westerncode.com" rel="noopener noreferrer">Western Code
+          <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
+        </Link>
       </div>
-
     </footer>
   );
 }
