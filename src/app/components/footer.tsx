91b101eaf1a2f7cc95a5a8f6165ddbad8c6e7814
Matt Steele <mattsteele000@gmail.com>
2025-03-24 18:14:39 -0600

    Update packages

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -11,13 +11,13 @@ export default function Footer() {
       </div>
 
       <div className="bg-red-one relative">
-        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/" rel="noopener noreferrer">About
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/about" rel="noopener noreferrer">About
           <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
         </Link>
       </div>
 
       <div className="bg-blue-two relative">
-        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/" rel="noopener noreferrer">Resume
+        <Link className="flex w-full justify-center py-2 uppercase items-center no-underline hover:*:w-full" href="/resume" rel="noopener noreferrer">Resume
           <span className="bg-gray-300 absolute bottom-0 left-0 block w-0 h-1 transparent transition-width duration-400"></span>
         </Link>
       </div>
